5070fda30f2cb753bdf27b77946b4787d3e6b7de
uvcorn <freelancermdshadhinali@gmail.com>
2021-12-18 11:50:04 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('App'),),
+      ),
     );
   }
 }
